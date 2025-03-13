f5a1293bdd431741fe18664bac08865d557561fe
Parrotcoding <128106128+Parrotcoding@users.noreply.github.com>
2025-03-13 18:23:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let zoomLevels = [
-    { start: 3.0, end: 4.0, interval: 0.1, decimals: 1 },
-    { start: 3.1, end: 3.2, interval: 0.01, decimals: 2 },
-    { start: 3.14, end: 3.15, interval: 0.001, decimals: 3 },
-    { start: 3.141, end: 3.142, interval: 0.0001, decimals: 4 },
-    { start: 3.1415, end: 3.1416, interval: 0.00001, decimals: 5 },
-    { start: 3.14159, end: 3.14160, interval: 0.000001, decimals: 6 },
-    { start: 3.141592, end: 3.141593, interval: 0.0000001, decimals: 7 }
-];
+const PI_100_DIGITS = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+
+let zoomLevels = [...Array.from({ length: 100 }, (_, i) => {
+    const decimals = i + 1;
+    const piValue = parseFloat(PI_100_DIGITS.substring(0, decimals + 2));
+    return {
+        start: piValue - Math.pow(10, -decimals),
+        end: piValue + Math.pow(10, -decimals),
+        interval: Math.pow(10, -decimals),
+        decimals: decimals
+    };
+})];
 
 let zoomIndex = 0;
-const pi = 3.1415926535;
 const numberLine = document.getElementById("numberLine");
 
 function drawNumberLine() {
@@ -38,6 +40,7 @@ function drawNumberLine() {
     }
 
     // Add π symbol at the correct position
+    let pi = parseFloat(PI_100_DIGITS.substring(0, decimals + 2));
     let piPosition = ((pi - start) / (end - start)) * width;
     let piSymbol = document.createElement("div");
     piSymbol.className = "pi-symbol";
